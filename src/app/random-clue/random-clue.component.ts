27c96c5de34894abdc3a0daa288f01086f339f0a
Nicolás Frati <nico@churrasco.cc>
2020-01-18 12:01:30 -0300

    random clue component created

---
+++ src/app/random-clue/random-clue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-clue',
+  templateUrl: './random-clue.component.html',
+  styleUrls: ['./random-clue.component.sass']
+})
+export class RandomClueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
