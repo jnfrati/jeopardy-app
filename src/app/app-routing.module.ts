d670ca2a4420164ef96a4fdc7a4e55e31a913d39
Nicolás Frati <nico@churrasco.cc>
2020-01-18 12:18:45 -0300

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RandomClueComponent } from './random-clue/random-clue.component';
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { CategoryComponent } from './category/category.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RandomClueComponent },
+  { path: 'categories', component:CategoriesListComponent },
+  { path: 'categories/:id', component: CategoryComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomClueComponent } from './random-clue/random-clue.component';
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomClueComponent,
+    CategoriesListComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
